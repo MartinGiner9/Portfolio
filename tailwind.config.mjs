f6f518bb8b1acd06906873e269a7260da59c06f0
MartinGiner9 <100756709+MartinGiner9@users.noreply.github.com>
2024-04-15 07:40:22 +0200

    Update tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,21 @@ export default {
         extend: {
             colors: {
                 primary: 'var(--color-primary)',
-                secondary: 'var(--color-secondary)'
+                secondary: 'var(--color-secondary)',
+                glacier: {
+                    '50': '#f1f8fa',
+                    '100': '#dcecf1',
+                    '200': '#bddae4',
+                    '300': '#77b0c680',
+                    '400': '#5b9ab5',
+                    '500': '#3f7e9b',
+                    '600': '#376883',
+                    '700': '#32566c',
+                    '800': '#30495a',
+                    '900': '#2b3e4e',
+                    '950': '#192833',
+                },
+    
             }
         },
     },
